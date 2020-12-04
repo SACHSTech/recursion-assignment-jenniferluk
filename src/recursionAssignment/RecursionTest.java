997d2fa2917801fae9cf4427e5d7b76e7d75951a
Jennifer Luk <jennifer.luk21@ycdsbk12.ca>
2020-12-04 01:56:09 +0000

    Renamed test names for better organization

--- src/recursionAssignment/RecursionTest.java
+++ src/recursionAssignment/RecursionTest.java
@@ -17,96 +17,103 @@ public class RecursionTest{
     
     // Check if Base Case works
     @Test
-    public void Test1(){
+    public void Test1_1(){
       // make assertion statement(s)
       assertEquals(0,Recursion.count8(0));
     }
     
      
     @Test
-    public void Test2(){
+    public void Test1_2(){
       // make assertion statement(s)
       assertEquals(1,Recursion.count8(8));
     }
 
     @Test
-    public void Test3(){
+    public void Test1_3(){
       // make assertion statement(s)
       assertEquals(2,Recursion.count8(818));
     }  
 
     @Test
-    public void Test4(){
+    public void Test1_4(){
       // make assertion statement(s)
       assertEquals(3,Recursion.count8(8818));
     }  
 
     // Test a case without 8
     @Test
-    public void Test5(){
+    public void Test1_5(){
       // make assertion statement(s)
       assertEquals(0,Recursion.count8(126));
+    } 
+
+    // Tests Complex Case
+    @Test
+    public void Test1_6(){
+      // make assertion statement(s)
+      assertEquals(6,Recursion.count8(1234886767318978868));
     }  
 
     // Problem 2 Tests
     
     // Check if Base Case works
     @Test
-    public void Test6(){
+    public void Test2_1(){
       // make assertion statement(s)
       assertEquals("p",Recursion.changePi("p"));
     }  
 
     @Test
-    public void Test7(){
+    public void Test2_2(){
       // make assertion statement(s)
       assertEquals("x3.14x",Recursion.changePi("xpix"));
     }  
 
     @Test
-    public void Test8(){
+    public void Test2_3(){
       // make assertion statement(s)
       assertEquals("3.143.14",Recursion.changePi("pipi"));
     }  
 
     @Test
-    public void Test9(){
+    public void Test2_4(){
       // make assertion statement(s)
       assertEquals("3.14p",Recursion.changePi("pip"));
     }  
 
     // Tests complex case
     @Test
-    public void Test9_1(){
+    public void Test2_5(){
       // make assertion statement(s)
       assertEquals("3.14pxxx3.14abc3.14paa",Recursion.changePi("pipxxxpiabcpipaa"));
     }  
 
     // Problem 3 Tests
     @Test
-    public void Test10(){
+    public void Test3_1(){
         // make assertion statement(s)
         assertTrue(Recursion.array220(new int[] {1, 2, 20},0));
     } 
 
     @Test
-    public void Test11(){
+    public void Test3_2(){
         // make assertion statement(s)
         assertFalse(Recursion.array220(new int[] {},0));
     }  
 
     @Test
-    public void Test12(){
+    public void Test3_3(){
         // make assertion statement(s)
         assertTrue(Recursion.array220(new int[] {3, 30},0));
     }  
 
     @Test
-    public void Test13(){
+    public void Test3_4(){
         // make assertion statement(s)
         assertFalse(Recursion.array220(new int[] {3},0));
     }  
-    
+
     @After
     public void afterTest(){
       // some steps to perform after tests are run
